2a60d4aae1b739ef92abce97ea5b243053a92925
filipnest <filip@bluejumpers.com>
2020-09-06 14:17:22 +0100

    Update web example

--- fiftyone_pipeline_devicedetection/examples/cloud/web.py
+++ fiftyone_pipeline_devicedetection/examples/cloud/web.py
@@ -45,8 +45,7 @@ if resourceKey == "!!YOUR_RESOURCE_KEY!!":
     You need to create a resource key at
     https://configure.51degrees.com and paste it into the code,
     'replacing !!YOUR_RESOURCE_KEY!!
-    Include the hardwarename and screenpixelswidth properties for the
-    example to work
+    To get a resourcekey with the properties used in this example go to https://configure.51degrees.com/fzzSBD8R
     """)
 else:
 
@@ -84,6 +83,18 @@ else:
 
         return json.dumps(flowData.jsonbundler.json)
 
+    # Helper function to get a property value if it exists and return 
+    # the reason why if it doesn't
+    def getValueHelper(flowdata, engine, propertyKey):
+
+        engineProperties = getattr(flowdata, engine)
+
+        propertyValue = getattr(engineProperties, propertyKey)
+
+        if propertyValue.hasValue():
+            return propertyValue.value()
+        else:
+            return propertyValue.noValueMessage
 
     # In the main route we dynamically update the screen's device property display
     # using the above JSON route
@@ -105,24 +116,54 @@ else:
 
         # Generate the HTML
 
-        output = ""
+        output = "<h1>Client side evidence</h1>"
 
         # Add the JavaScript created by the pipeline
         output += "<script>"
         output += flowData.javascriptbuilder.javascript
         output += "</script>"
 
+        # Print results from server side processing
+        output += "<p><b>The following values are determined by sever-side device detection on the first request:</b></p>"
+
+        output += "<b>Hardware Vendor:</b> " + getValueHelper(flowData, "device", "hardwarevendor")
+        output += "<br />"
+        output += "<b>Hardware Name:</b> " + str(getValueHelper(flowData, "device", "hardwarename"))
+        output += "<br />"
+        output += "<b>Device Type:</b> " + getValueHelper(flowData, "device", "devicetype")
+        output += "<br />"
+        output += "<b>Platform Vendor:</b> " + getValueHelper(flowData, "device", "platformvendor")
+        output += "<br />"
+        output += "<b>Platform Name:</b> " + getValueHelper(flowData, "device", "platformname")
+        output += "<br />"
+        output += "<b>Browser Vendor:</b> " + getValueHelper(flowData, "device", "browservendor")
+        output += "<br />"
+        output += "<b>Browser Name:</b> " + getValueHelper(flowData, "device", "browsername")
+        output += "<br />"
+        output += "<b>Browser Version:</b> " + getValueHelper(flowData, "device", "browserversion")
+        output += "<br />"
+        output += "<b>Screen width (pixels):</b> " + str(getValueHelper(flowData, "device", "screenpixelswidth"))
+        output += "<br />"
+        output += "<b>Screen height (pixels):</b> " + str(getValueHelper(flowData, "device", "screenpixelsheight"))
+     
         ## Print results of client side processing to the page.
         output += """
+
+        <p>The information shown below is determined from JavaScript running on the client-side that is able to obtain additional evidence. If no additional information appears then it may indicate an external problem such as JavaScript being disabled in your browser.</p>
+
+        <p>Note that the 'Hardware Name' field is intended to illustrate detection of Apple device models as this cannot be determined server-side. This can be tested to some extent using most emulators such as those in the 'developer tools' menu in Google Chrome. However, using real devices will result in more precise model numbers.</p>
+
               <p id=hardwarename></p>
               <p id=screenpixelwidth></p>
+              <p id=screenpixelheight></p>
               <script>
               window.onload = function(){
                 fod.complete(function (data) {
                       if(data.device["hardwarename"]){
                       document.getElementById('hardwarename').innerHTML = "<strong>Updated Hardware Name from client-side evidence:</strong> " + data.device["hardwarename"];
                       }
-                      document.getElementById('screenpixelwidth').innerHTML = "<strong>Pixel width: " + data.device.screenpixelswidth + "</strong>"
+                      document.getElementById('screenpixelwidth').innerHTML = "<strong>Screen width (pixels): " + data.device.screenpixelswidth + "</strong>"
+                      document.getElementById('screenpixelheight').innerHTML = "<strong>Screen height (pixels): " + data.device.screenpixelsheight + "</strong>"
                     });
               }
               </script>
