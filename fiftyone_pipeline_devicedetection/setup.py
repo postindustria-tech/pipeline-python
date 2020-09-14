18c22d29cb25a2e073ce12b609ca12e32535a369
filipnest <filip@bluejumpers.com>
2020-09-14 08:29:16 +0100

    Split out device detection and location

--- fiftyone_pipeline_devicedetection/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="fiftyone_pipeline_devicedetection",
-    version="0.0.1",
-    author="51Degrees",
-    python_requires='>=2.7',
-    packages=["fiftyone_pipeline_devicedetection"]
-)
