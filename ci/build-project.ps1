b365eb5c21243904a1f0a92aca12e95b2923596b
Alexander Popel <alexander.popel@postindustria.com>
2023-05-22 13:52:45 +0300

    Add the list of packages to build-project.ps1

--- ci/build-project.ps1
+++ ci/build-project.ps1
@@ -3,6 +3,7 @@ param (
     [string]$RepoName
 )
 
-./python/build-project.ps1 -RepoName $RepoName
+$packages = "fiftyone_pipeline_core", "fiftyone_pipeline_engines", "fiftyone_pipeline_engines_fiftyone", "fiftyone_pipeline_cloudrequestengine"
+./python/build-project.ps1 -RepoName $RepoName -Packages $packages
 
 exit $LASTEXITCODE
