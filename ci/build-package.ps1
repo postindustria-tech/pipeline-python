ee4caae64f16df39e3e35d2f1a9da038177c5816
Alexander Popel <alexander.popel@postindustria.com>
2023-05-23 11:55:52 +0300

    Skip setup-environment step in PreBuild CI stage

--- ci/build-package.ps1
+++ ci/build-package.ps1
@@ -6,6 +6,6 @@ param (
 )
 
 $packages = "fiftyone_pipeline_core", "fiftyone_pipeline_engines", "fiftyone_pipeline_engines_fiftyone", "fiftyone_pipeline_cloudrequestengine"
-./python/build-package.ps1 -RepoName $RepoName -Version $Version -Packages $packages
+./python/build-package-pypi.ps1 -RepoName $RepoName -Version $Version -Packages $packages
 
 exit $LASTEXITCODE
--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -3,6 +3,12 @@ param (
     [string]$LanguageVersion
 )
 
+Write-Output "GITHUB_JOB: $env:GITHUB_JOB"
+if ($env:GITHUB_JOB -eq "PreBuild") {
+    Write-Output "Skipping environment setup"
+    exit 0
+}
+
 $dependencies = "pylint", "unittest-xml-reporting", "coverage", "parameterized", "requests-mock", "flask"
 ./python/setup-environment.ps1 -LanguageVersion $LanguageVersion -Dependencies $dependencies
 
