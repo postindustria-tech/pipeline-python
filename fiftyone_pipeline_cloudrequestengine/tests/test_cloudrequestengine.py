9609cd10f76acc275919a73084620daeb9559a14
Bohdan <sniperik@users.noreply.github.com>
2023-11-10 21:19:00 +0200

    Improve code quality/fix linter warnings

--- fiftyone_pipeline_cloudrequestengine/tests/test_cloudrequestengine.py
+++ fiftyone_pipeline_cloudrequestengine/tests/test_cloudrequestengine.py
@@ -34,7 +34,6 @@ from .classes.cloudrequestengine_testbase import CloudRequestEngineTestsBase
 
 
 class TestCloudRequestEngine(CloudRequestEngineTestsBase):
-
     """!
     Test cloud request engine adds correct information to post request
     following the order of precedence when processing evidence and 
@@ -47,55 +46,46 @@ class TestCloudRequestEngine(CloudRequestEngineTestsBase):
     testEnvVarEndPoint = "http://testEnvVarEndPoint/"
 
     def setUp(self):
-        if os.environ.get(Constants.FOD_CLOUD_API_URL) != None:
+        if os.environ.get(Constants.FOD_CLOUD_API_URL) is not None:
             del os.environ[Constants.FOD_CLOUD_API_URL]
 
     def testConfigEndPoint_ExplicitSetting(self):
-
         """!
         Check that the explicitly setting the cloud endpoint via 
         method interface take precedence over everything else
         """
 
-        httpClient = self.mock_http()
-
         os.environ[Constants.FOD_CLOUD_API_URL] = TestCloudRequestEngine.testEnvVarEndPoint
-        cloudRequestEngine = CloudRequestEngine({
+        cloud_request_engine = CloudRequestEngine({
             "resource_key": TestCloudRequestEngine.testResourceKey,
             "cloud_endpoint": TestCloudRequestEngine.testEndPoint,
-            "http_client": httpClient
+            "http_client": self.mock_http()
         })
-        self.assertEqual(TestCloudRequestEngine.testEndPoint, cloudRequestEngine.baseURL)
+        self.assertEqual(TestCloudRequestEngine.testEndPoint, cloud_request_engine.baseURL)
 
     def testConfigEndPoint_EnvironmentVariableSetting(self):
-
         """!
         Check that the setting the cloud endpoint via environment 
         variable take precedence over the default value
         """
 
-        httpClient = self.mock_http()
-
         os.environ[Constants.FOD_CLOUD_API_URL] = TestCloudRequestEngine.testEnvVarEndPoint
-        cloudRequestEngine = CloudRequestEngine({
+        cloud_request_engine = CloudRequestEngine({
             "resource_key": TestCloudRequestEngine.testResourceKey,
-            "http_client": httpClient
+            "http_client": self.mock_http()
         })
-        self.assertEqual(TestCloudRequestEngine.testEnvVarEndPoint, cloudRequestEngine.baseURL)
+        self.assertEqual(TestCloudRequestEngine.testEnvVarEndPoint, cloud_request_engine.baseURL)
 
     def testConfigEndPoint_DefaultSetting(self):
-
         """!
         Check that if nothing else is set the default value should be used
         """
 
-        httpClient = self.mock_http()
-
-        cloudRequestEngine = CloudRequestEngine({
+        cloud_request_engine = CloudRequestEngine({
             "resource_key": TestCloudRequestEngine.testResourceKey,
-            "http_client": httpClient
+            "http_client": self.mock_http()
         })
-        self.assertEqual(Constants.BASE_URL_DEFAULT, cloudRequestEngine.baseURL)
+        self.assertEqual(Constants.BASE_URL_DEFAULT, cloud_request_engine.baseURL)
 
     @parameterized.expand([
         (False, "query.User-Agent=iPhone", "header.User-Agent=iPhone"),
@@ -106,7 +96,6 @@ class TestCloudRequestEngine(CloudRequestEngineTestsBase):
         (True, "e.value=1", "f.value=1")
     ])
     def test_evidence_precedence(self, warn, evidence1, evidence2):
-
         """!
         Test cloud request engine adds correct information to post request
         following the order of precedence when processing evidence and 
@@ -114,21 +103,21 @@ class TestCloudRequestEngine(CloudRequestEngineTestsBase):
         should be added in descending order of precedence.
         """
 
-        evidence1Parts = evidence1.split("=")
-        evidence2Parts = evidence2.split("=")
-
-        httpClient = self.mock_http()
+        evidence1_parts = evidence1.split("=")
+        evidence2_parts = evidence2.split("=")
         
-        engine = CloudRequestEngine({"resource_key": TestConstants.resourceKey,
-            "http_client" : httpClient})
+        engine = CloudRequestEngine({
+            "resource_key": TestConstants.resourceKey,
+            "http_client": self.mock_http(),
+        })
 
-        builder= PipelineBuilder();
+        builder = PipelineBuilder()
         pipeline = builder.add(engine).build()
 
         data = pipeline.create_flowdata()
 
-        data.evidence.add(evidence1Parts[0], evidence1Parts[1])
-        data.evidence.add(evidence2Parts[0], evidence2Parts[1])
+        data.evidence.add(evidence1_parts[0], evidence1_parts[1])
+        data.evidence.add(evidence2_parts[0], evidence2_parts[1])
 
         with warnings.catch_warnings(record=True) as w:
             # Cause all warnings to always be triggered.
@@ -142,40 +131,51 @@ class TestCloudRequestEngine(CloudRequestEngineTestsBase):
             # Verify warning is thrown.
             self.assertTrue(len(w) == 1)
             self.assertTrue(issubclass(w[-1].category, UserWarning))
-            self.assertTrue(TestConstants.PRECENDENCE_WARNING  \
-                    .format(evidence1Parts[0], evidence1Parts[1], \
-                    evidence2Parts[0], evidence2Parts[1]) \
-                    in str(w[-1].message))
+            self.assertIn(
+                TestConstants.PRECENDENCE_WARNING.format(
+                    evidence1_parts[0],
+                    evidence1_parts[1],
+                    evidence2_parts[0],
+                    evidence2_parts[1]
+                ),
+                str(w[-1].message),
+            )
         else:
             self.assertTrue(len(w) == 0)
 
     @parameterized.expand([
-        ({"query.User-Agent":"iPhone", "header.User-Agent":"iPhone"}, "query", {"query.User-Agent":"iPhone"}),
-        ({"header.User-Agent":"iPhone", "a.User-Agent":"iPhone", "z.User-Agent":"iPhone"}, "other", {"z.User-Agent":"iPhone", "a.User-Agent":"iPhone"})
+        (
+            {"query.User-Agent": "iPhone", "header.User-Agent": "iPhone"},
+            "query",
+            {"query.User-Agent": "iPhone"},
+        ),
+        (
+            {"header.User-Agent": "iPhone", "a.User-Agent": "iPhone", "z.User-Agent": "iPhone"},
+            "other",
+            {"z.User-Agent": "iPhone", "a.User-Agent": "iPhone"},
+        ),
     ])
-    def test_get_selected_evidence(self, evidence, type, expected_value):
-
+    def test_get_selected_evidence(self, evidence, ev_type, expected_value):
         """!
         Test evidence of specific type is returned from all 
         the evidence passed, if type is not from query, header
         or cookie then evidences are returned sorted in descensing order
         """
 
-        httpClient = self.mock_http()
-        
-        engine = CloudRequestEngine({"resource_key": TestConstants.resourceKey,
-            "http_client" : httpClient})
+        engine = CloudRequestEngine({
+            "resource_key": TestConstants.resourceKey,
+            "http_client": self.mock_http(),
+        })
 
-        result = engine.get_selected_evidence(evidence, type)
+        result = engine.get_selected_evidence(evidence, ev_type)
         self.assertEqual(expected_value, result)
 
     @parameterized.expand([
-        ({"query.User-Agent":"query-iPhone", "header.User-Agent":"header-iPhone"},  "query-iPhone"),
-        ({"header.User-Agent":"header-iPhone", "cookie.User-Agent":"cookie-iPhone"}, "header-iPhone"),
-        ({"a.User-Agent":"a-iPhone", "b.User-Agent":"b-iPhone", "z.User-Agent":"z-iPhone"}, "a-iPhone")
+        ({"query.User-Agent": "query-iPhone", "header.User-Agent": "header-iPhone"},  "query-iPhone"),
+        ({"header.User-Agent": "header-iPhone", "cookie.User-Agent": "cookie-iPhone"}, "header-iPhone"),
+        ({"a.User-Agent": "a-iPhone", "b.User-Agent": "b-iPhone", "z.User-Agent": "z-iPhone"}, "a-iPhone")
     ])
     def test_get_content(self, evidence, expected_value):
-
         """!
         Test Content to send in the POST request is generated as
         per the precedence rule of The evidence keys. These are
@@ -183,12 +183,12 @@ class TestCloudRequestEngine(CloudRequestEngineTestsBase):
         the queryData value is overwritten.
         """
 
-        httpClient = self.mock_http()
-        
-        engine = CloudRequestEngine({"resource_key": TestConstants.resourceKey,
-            "http_client" : httpClient})
+        engine = CloudRequestEngine({
+            "resource_key": TestConstants.resourceKey,
+            "http_client": self.mock_http(),
+        })
 
-        builder= PipelineBuilder();
+        builder = PipelineBuilder()
         pipeline = builder.add(engine).build()
 
         data = pipeline.create_flowdata()
