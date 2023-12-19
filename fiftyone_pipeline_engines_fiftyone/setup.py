d48ffd63242161fd0766588e638d6f383ab10acd
Victor Malitskyi <victor.malitsky@postindustria.com>
2023-12-19 21:50:54 +0100

    Updated packages author

--- fiftyone_pipeline_engines_fiftyone/setup.py
+++ fiftyone_pipeline_engines_fiftyone/setup.py
@@ -38,7 +38,7 @@ def read(file_name):
 setuptools.setup(
     name="fiftyone_pipeline_engines_fiftyone",
     version=read("version.txt"),
-    author="51Degrees",
+    author="51Degrees Engineering",
     author_email="support@51degrees.com",
     url="https://51degrees.com/",
     description=("The 51Degrees Pipeline API is a generic web request intelligence and data processing solution with the ability to add a range of 51Degrees and/or custom plug ins (Engines). "
