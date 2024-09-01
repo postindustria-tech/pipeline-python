c1aa97f70aaf2777f81c78b32fe329f5a4a91e01
Automation51D <51DCI@51Degrees.com>
2024-09-01 00:19:05 +0000

    REF: Updated copyright.

--- fiftyone_pipeline_engines/setup.py
+++ fiftyone_pipeline_engines/setup.py
@@ -18,7 +18,7 @@
 # including the attribution notice(s) required under Article 5 of the EUPL
 # in the end user terms of the application under an appropriate heading,
 # such notice(s) shall fulfill the requirements of that article.
-# ********************************************************************* 
+# *********************************************************************
 
 import setuptools
 import os
